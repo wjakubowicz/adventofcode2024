333da0dec4124922e985c8cf7653f3c107f1e9d7
wjakubowicz <wjakubowicz@duck.com>
2024-12-02 22:49:41 +0100

    Add README and Day 2 puzzle solution

---
+++ README.md
@@ -0,0 +1,28 @@
+# Advent of Code 2024
+
+This project contains my solutions for the Advent of Code 2024 challenges.
+
+## About
+
+Advent of Code is an annual event in December where participants solve daily programming puzzles. It's a great way to improve your coding skills and have fun with the programming community.
+
+## Structure
+
+Each day's challenge will be organized in its own directory:
+
+```
+/day01
+/day02
+...
+```
+
+Each directory will contain:
+- my solution for the puzzle
+- puzzle description
+- input for the puzzle generated on a per-user basis
+
+## Acknowledgements
+
+- [Advent of Code](https://adventofcode.com/) by Eric Wastl
+
+Happy coding!
---
+++ day02/advent02.py
@@ -0,0 +1,24 @@
+def is_safe_report(report):
+    increasing = all(report[i] < report[i + 1] and 1 <= report[i + 1] - report[i] <= 3 for i in range(len(report) - 1))
+    decreasing = all(report[i] > report[i + 1] and 1 <= report[i] - report[i + 1] <= 3 for i in range(len(report) - 1))
+    return increasing or decreasing
+
+def is_safe_with_dampener(report):
+    if is_safe_report(report):
+        return True
+    for i in range(len(report)):
+        modified_report = report[:i] + report[i+1:]
+        if is_safe_report(modified_report):
+            return True
+    return False
+
+def count_safe_reports(data):
+    reports = [list(map(int, line.split())) for line in data.strip().split('\n')]
+    safe_count = sum(1 for report in reports if is_safe_with_dampener(report))
+    return safe_count
+
+with open('advent02.txt', 'r') as file:
+    data = file.read()
+
+safe_reports_count = count_safe_reports(data)
+print(f"Number of safe reports: {safe_reports_count}")
