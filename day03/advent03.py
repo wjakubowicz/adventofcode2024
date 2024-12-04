02efa8628129a49814a7894afce69edf2dbb3a26
wjakubowicz <wjakubowicz@duck.com>
2024-12-04 18:23:15 +0100

    Add Day 3 puzzle solution

---
+++ day03/advent03.py
@@ -0,0 +1,28 @@
+import re
+
+def part1(data):
+    pattern = r"mul\((\d+),(\d+)\)"
+    matches = re.findall(pattern, data)
+    return sum(int(x) * int(y) for x, y in matches)
+
+def part2(data):
+    enabled = True
+    result = 0
+    pattern = r"(mul\((\d+),(\d+)\)|do\(\)|don't\(\))"
+    instructions = re.findall(pattern, data)
+    
+    for inst in instructions:
+        if inst[0] == "do()":
+            enabled = True
+        elif inst[0] == "don't()":
+            enabled = False
+        else:
+            if enabled:
+                result += int(inst[1]) * int(inst[2])
+            
+    return result
+
+with open('advent03.txt') as f:
+	data = f.read()
+	print(part1(data))
+	print(part2(data))
